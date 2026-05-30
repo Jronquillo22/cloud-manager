67df151f4b04f81c2d867f26b96f60317cc7cccf
Jimmy Ronquillo <jronquilloq@miumg.edu.gt>
2026-05-30 04:04:39 -0600

    actualizacion web

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let selectedRole = "user";
+
 function showRegister() {
   document.getElementById("loginBox").classList.add("hidden");
   document.getElementById("registerBox").classList.remove("hidden");
@@ -8,14 +10,18 @@ function showLogin() {
   document.getElementById("loginBox").classList.remove("hidden");
 }
 
+function setRole(role) {
+  selectedRole = role;
+  document.getElementById("roleSelected").innerText = "Rol: " + role;
+}
+
 function register() {
   let user = document.getElementById("regUser").value;
   let pass = document.getElementById("regPass").value;
-  let role = document.getElementById("regRole").value;
 
   let users = JSON.parse(localStorage.getItem("users") || "[]");
 
-  users.push({ user, pass, role });
+  users.push({ user, pass, role: selectedRole });
 
   localStorage.setItem("users", JSON.stringify(users));
 
@@ -41,9 +47,24 @@ function login() {
 
   if (found.role === "admin") {
     document.getElementById("adminPanel").classList.remove("hidden");
+
+    loadUsers();
   } else {
     document.getElementById("userPanel").classList.remove("hidden");
   }
+}
+
+function loadUsers() {
+  let users = JSON.parse(localStorage.getItem("users") || "[]");
+  let list = document.getElementById("userList");
+
+  list.innerHTML = "";
+
+  users.forEach(u => {
+    let li = document.createElement("li");
+    li.innerText = `${u.user} - ${u.role}`;
+    list.appendChild(li);
+  });
 }
 
 function logout() {
